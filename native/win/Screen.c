03da843b83fa4c75bdb7f712b32655b6f5500596
Greg Peters <gregpeters00@gmail.com>
2021-02-08 23:44:25 -0500

    removed unused comments

--- native/win/Screen.c
+++ native/win/Screen.c
@@ -26,14 +26,14 @@ BOOL CALLBACK EnumMonitorsCallback(HMONITOR hMonitor, HDC hdcMonitor, LPRECT lpr
     uint32_t builtIn = x == 0 && y == 0?1:0;
     uint32_t mainDisplay = iMonitor.dwFlags == MONITORINFOF_PRIMARY;
 
-   // setNumberProperty(env,&screen,CGDisplayModelNumber(curScreen),"modelNumber");
+   // setNumberProperty(env,&screen,0,"modelNumber"); //add model number
     setNumberProperty(gEnv,&screen,builtIn,"isBuiltIn");
     setNumberProperty(gEnv,&screen,mainDisplay,"isMainDisplay");
     setNumberProperty(gEnv,&screen,width,"width");
     setNumberProperty(gEnv,&screen,height,"height");
     setNumberProperty(gEnv,&screen,x,"x");
     setNumberProperty(gEnv,&screen,y,"y");
-   // setNumberProperty(env,&screen,(double)CGDisplayRotation(curScreen),"rotation");
+   // setNumberProperty(env,&screen,0,"rotation"); //add rotation
     napi_set_element(gEnv,screens,screenIdx++,screen);
     return TRUE;
 }
@@ -51,21 +51,8 @@ napi_status _getDisplayInfo(napi_env env,napi_value* display) {
         napi_throw_error(env,NULL,"Unable to create native NodeJS array for screens");
         return status;
     }
+
     EnumDisplayMonitors(NULL, NULL, &EnumMonitorsCallback, 0);
-    // for (int i = 0; i < (int)screenCount; i++) {
-    //     napi_value screen;
-    //     createObject(env,&screen);
-    //     CGDirectDisplayID curScreen = screenList[i];
-    //     setNumberProperty(env,&screen,CGDisplayModelNumber(curScreen),"modelNumber");
-    //     setNumberProperty(env,&screen,CGDisplayIsBuiltin(curScreen),"isBuiltIn");
-    //     setNumberProperty(env,&screen,CGDisplayIsMain(curScreen),"isMainDisplay");
-    //     setNumberProperty(env,&screen,(int)CGDisplayPixelsWide(curScreen),"width");
-    //     setNumberProperty(env,&screen,(int)CGDisplayPixelsHigh(curScreen),"height");
-    //     setNumberProperty(env,&screen,(int)CGDisplayBounds(curScreen).origin.x,"x");
-    //     setNumberProperty(env,&screen,(int)CGDisplayBounds(curScreen).origin.y,"y");
-    //     setNumberProperty(env,&screen,(double)CGDisplayRotation(curScreen),"rotation");
-    //     napi_set_element(env,screens,i,screen);
-    // }
 
     status = napi_set_named_property(env, *display, "screens", screens);
     if(status != napi_ok) {
